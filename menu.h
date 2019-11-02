f59abd87344bb09c0f7d54216f6b6ad1e253dce1
Eduardo Nicoletti <JKGaming4@outlook.com>
2019-11-02 16:04:18 -0300

    atualizado o menu

--- menu.h
+++ menu.h
@@ -6,7 +6,7 @@ int menu(){
   printf("\t1 - Central de Vendas \n");
   printf("\t2 - Controle de Estoque \n");
   printf("\t3 - Sistema Financeiro \n");
-  printf("\t4 - Controle De Estoque \n");
+  printf("\t4 - Sair \n");
   scanf("%d", &escolha);
 
   return escolha;
