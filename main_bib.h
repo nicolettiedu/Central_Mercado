88ce9f4636d22dec4c898a8d2d92159cf57d534a
hectorhu17 <hectorhu17@gmail.com>
2019-11-03 16:36:47 -0200

    Atualização da biblioteca estoque.h No main.c adicionado a biblioteca estoque.h, adicionado a variavel produtos adicionado o README.md

--- main_bib.h
+++ main_bib.h
@@ -13,11 +13,11 @@ int menu(){
   int escolha;
 
   printf("===============================================================\n");
-  printf("============ Bem-vindo a Central de Vendas! ===================\n\n");
-  printf("\t1 - Central de Vendas \n");
-  printf("\t2 - Controle de Estoque \n");
-  printf("\t3 - Sistema Financeiro \n");
-  printf("\t4 - Sair \n");
+  printf("============ BEM-VINDO A CENTRAL DE VENDAS! ===================\n\n");
+  printf("\t1 - CENTRAL DE VENDAS \n");
+  printf("\t2 - CONTROLE DE ESTOQUE \n");
+  printf("\t3 - SISTEMA FINANCEIRO \n");
+  printf("\t4 - SAIR \n");
   scanf("%d", &escolha);
 
   return escolha;
